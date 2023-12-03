01d67801bacf0d03d874a2dcc32ec5b03ac82089
Brian McCallister <brianm@skife.org>
2023-12-02 17:45:48 -0800

    parsing!

--- aoc2023/day2.py
+++ aoc2023/day2.py
@@ -1,11 +1,12 @@
+from collections.abc import Sequence
 import sys
 import re
 from parsimonious import Grammar, NodeVisitor
 
 grammar = Grammar(
     r"""
-    game = GameId ws ":" ws (draw ";"? ws)+ 
-    GameId = "Game" ws ~"[0-9]+"        
+    game = gameId ws ":" ws (draw ";"? ws)+ 
+    gameId = "Game" ws ~"[0-9]+"        
     draw = (marbles ","? ws)+ 
     marbles = number ws color ws
     number = ("0"/"1"/"2"/"3"/"4"/"5"/"6"/"7"/"8"/"9")+
@@ -15,14 +16,41 @@ grammar = Grammar(
 )
 
 class Part1Visitor(NodeVisitor):
-    def visit_game(self, node, visited_children):
-        print("visit_game", node, visited_children)
+    def visit_game(self, node, visited_children):        
+        game_id, _, _, _, draws = visited_children              
+        draws = [draw for draw, _, _ in draws]            
+        return game_id, draws
+    
+    def visit_gameId(self, node, visited_children):
+        return node.text
+    
+    def visit_draw(self, node, visited_children):
+        return [draw for draw, _, _ in visited_children]
+    
+    def visit_marbles(self, node, visited_children):
+        num, _, color, _= visited_children        
+        return (num, color)
+
+    def visit_number(self, node, visited_children):
+        return int(node.text)
+
+    def visit_color(self, node, visited_children):        
+        return node.text
+
+    def visit_ws(self, node, visited_children):
+        return None
+    
+    def generic_visit(self, node, visited_children):
+        return visited_children 
 
 def part1(data: str) -> int:
     for line in data.splitlines():
-        print(line)        
         v = Part1Visitor()
-        v.visit(grammar.parse(line))
+        tree = grammar.parse(line)
+        #print(tree)
+        #print()
+        game = v.visit(tree)
+        print(f"{game}")
     return 0
 
 
